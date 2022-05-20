4e20d08c789b45cb57933c708601ccd5133a5729
Tochukwu Jonah <jonahtochukwu@gmail.com>
2022-05-20 14:54:04 +0100

    Updated global styling

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">GGGGGGGGGGGhjhhjjhhj</div>;
+  return (
+    <div className="App">
+      sdsdsds
+    </div>
+  )
 }
 
 export default App;
