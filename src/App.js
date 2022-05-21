3fcd67ff33ff97516187b539723ff3e8a4bec586
Tochukwu Jonah <jonahtochukwu@gmail.com>
2022-05-21 09:55:03 -0500

    commented the images

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <HowItWorks />
+        <Footer />
       </Router>
     </div>
   );
--- src/components/footer/style.js
+++ src/components/footer/style.js
@@ -6,7 +6,6 @@ export const Container = styled.div`
   width: 100%;
   padding: 0.5rem calc((100vw - 1300px) / 2);
   margin: 0 auto;
-  border: 1px solid red;
 `;
 
 export const FooterWrapper = styled.div`
@@ -15,10 +14,13 @@ export const FooterWrapper = styled.div`
   justify-content: space-between;
   width: calc(100% - 0px);
   padding: 50px 0;
-  border: 1px solid red;
   margin: 0 auto;
   background-color: ${colors.primary };
   color: ${colors.white};
+
+  @media (max-width: 550px){
+    flex-direction: column-reverse;
+  }
 
   @media (max-width: 1000px) {
     flex-wrap: wrap;
@@ -28,16 +30,42 @@ export const FooterWrapper = styled.div`
   }
 `;
 
+export const IntroContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: start;
+
+  @media (max-width: 550px){
+    padding: 50px 20px;
+  }
+
+  @media (min: 551px) and (max-width: 911px){
+    padding: 50px 20px;
+  }
+`;
+
 export const FooterIntroWrap = styled.div`
   display: flex;
   flex-direction: row;
-  border: 2px solid red;
+  justify-content: space-between;
+
+  @media (max-width: 550px){
+    flex-wrap: wrap;
+    padding: 0 20px;
+  }
+
 `;
 
 export const FooterIntro = styled.p`
   max-width: 20.9rem;  
   width: fit-content;
   text-align: start;
+
+  @media(max-width: 550px){
+    max-width: max-content;  
+    width: fit-content;
+  }
 `;
 
 export const FooterLink = styled(Link)`
@@ -61,13 +89,20 @@ export const FooterLink = styled(Link)`
   }
 `;
 
+
 export const FooterMenu = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: start;
-  margin-right: 30px;
-  @media (max-width: 768px) {
+  margin-right: 80px;
+
+  @media (max-width: 550px){
+    padding-top: 20px;
+    margin-right: 30px;
+  }
+
+  @media (min-width: ;) (max-width: 768px) {
     display: none;
   }
 `;
@@ -76,21 +111,40 @@ export const MenuHeader = styled.p`
   padding-bottom: 1rem;
   font-size: 1rem;
   font-weight: 400;
+  font-family: 'Monument Extended' montserrat;
+  color: ${colors.white};
 `;
 
-export const FooterCredit = styled.div`
+export const FooterCreditCont = styled.div`
   display: flex;
-  padding: 20px 0px;
+  padding: 50px 0px;
   justify-content: space-between;
-  color: #A4A4A4;
+  color: ${colors.grey_text};
   font-family: 'DM Sans';
+
+  @media (max-width: 550px){
+    flex-direction: column;
+    // flex-wrap: wrap;  
+    justify-content: flex-start;
+    padding: 0 20px;
+    
+  }
+`;
+
+export const FooterCredit = styled.p`
+  text-align: start;
 `;
 
 export const FooterSocial = styled.div`
   display: flex;
   justify-content: space-between;
-  max-width: 7%;
+  max-width: 10%;
   width: 100%;
+
+  @media (max-width: 550px){
+    padding-top: 40px;
+    max-width: 45%;
+  }
 `;
 
 
