31832b37d25dffc00a2675ec3d831dc57534972a
mad-max-fury <chrisnonso8@gmail.com>
2022-05-23 20:31:52 +0100

    added road map

---
+++ src/pages/aboutpage/components/roadmap/index.js
@@ -0,0 +1,158 @@
+import React from "react";
+import styled from "styled-components";
+import { looperGroup } from "../../../../assets";
+import { colors } from "../../../../colors";
+const RoadMap = () => {
+  return (
+    <RoadMapContainer>
+      <div className="image-bg"></div>
+      <Heading>Our Road Map</Heading>
+      <MapContainer>
+        <div>
+          <div>
+            <MiniCardWrap>
+              <ItemHeading>Q1 - Launch Platform</ItemHeading>
+              <ItemParagraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio
+                imperdiet nibh at et neque.
+              </ItemParagraph>
+            </MiniCardWrap>
+          </div>
+        </div>
+        <div>
+          <div>
+            <MiniCardWrap>
+              <ItemHeading>Q1 - Launch Platform</ItemHeading>
+              <ItemParagraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio
+                imperdiet nibh at et neque.
+              </ItemParagraph>
+            </MiniCardWrap>
+          </div>
+        </div>
+        <div>
+          <div>
+            <MiniCardWrap>
+              <ItemHeading>Q1 - Launch Platform</ItemHeading>
+              <ItemParagraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio
+                imperdiet nibh at et neque.
+              </ItemParagraph>
+            </MiniCardWrap>
+          </div>
+        </div>
+        <div>
+          <div>
+            <MiniCardWrap>
+              <ItemHeading>Q1 - Launch Platform</ItemHeading>
+              <ItemParagraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Odio
+                imperdiet nibh at et neque.
+              </ItemParagraph>
+            </MiniCardWrap>
+          </div>
+        </div>
+      </MapContainer>
+    </RoadMapContainer>
+  );
+};
+
+export { RoadMap };
+
+const RoadMapContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  margin: 0 auto;
+  align-items: center;
+  width: 90%;
+  position: relative;
+  padding: 8rem calc((100vw - 1300px) / 2);
+  height: fit-content;
+  position: relative;
+  margin-top: 8rem;
+  & > div {
+    position: absolute;
+    height: calc(100% + 100px);
+    width: 100%;
+    top: 0;
+    background: url(${looperGroup});
+    z-index: -1;
+  }
+`;
+
+const Heading = styled.h2`
+  font-size: 42px;
+  font-weight: 500;
+  margin: 0 auto;
+`;
+const MapContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: 60rem;
+  padding: 6rem;
+  gap: 2rem;
+  & > div {
+    width: 5px;
+    height: 20rem;
+    position: relative;
+    &:before {
+      content: "";
+      width: 30px;
+      height: 3px;
+      position: absolute;
+      right: -250%;
+      top: -10%;
+      background-color: ${colors.secondary};
+    }
+    & > div {
+      position: absolute;
+      height: 100%;
+      width: 20rem;
+      top: 0;
+      border-left: 6px dashed #333;
+      border-right: unset;
+      border-bottom: unset;
+      border-top: unset;
+    }
+    &:nth-of-type(even) {
+      & > div {
+        border: 1px solid green;
+        left: -19.5rem;
+        border-right: 6px dashed #333;
+        border-left: unset;
+        border-bottom: unset;
+        border-top: unset;
+        display: flex;
+        & > div {
+          right: 7%;
+          left: unset;
+        }
+      }
+    }
+  }
+`;
+
+const MiniCardWrap = styled.div`
+  max-width: 199px;
+  margin: 0 auto;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
+  margin-top: 1.5rem;
+  position: absolute;
+  top: -25%;
+  left: 7%;
+  /* padding: 0 1.5rem; */
+`;
+
+export const ItemHeading = styled.h6`
+  text-align: start !important;
+`;
+export const ItemParagraph = styled.p`
+  text-align: start;
+  line-height: unset;
+`;
--- src/pages/aboutpage/index.js
+++ src/pages/aboutpage/index.js
@@ -1,55 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
-import { 
-    HeroSection,
-    ProjectSuccess,
-    Tokenomics, 
+import { HeroSection, ProjectSuccess, RoadMap, Tokenomics } from "./components";
 
-} from './components'
+import { colors } from "../../colors";
 
-import { colors } from '../../colors'
-
-import { Accordion, Allocation } from '../../components'
+import { Accordion, Allocation } from "../../components";
 
 const AboutPage = () => {
   return (
     <Container>
-        <HeroSection/>
-        <ProjectSuccess/>
-        <Tokenomics token={"Token Name"} pronto={"Defipronto"} marketcap={"Market Cap"} figure1={"$1,000,000"} initial={"Initial Supply"} figure2={"400,000"} current={"Current Total Supply"} figure3={"200,000"} burned={"Defipronto Burned"} figure4={"30,000"} circulation={"Defipronto in Circulation"} figure5={"100,000"} />
-        
-        <Allocation/>
+      <HeroSection />
+      <ProjectSuccess />
+      <RoadMap />
+      <Tokenomics
+        token={"Token Name"}
+        pronto={"Defipronto"}
+        marketcap={"Market Cap"}
+        figure1={"$1,000,000"}
+        initial={"Initial Supply"}
+        figure2={"400,000"}
+        current={"Current Total Supply"}
+        figure3={"200,000"}
+        burned={"Defipronto Burned"}
+        figure4={"30,000"}
+        circulation={"Defipronto in Circulation"}
+        figure5={"100,000"}
+      />
 
-        <AccordionContainer>
-            <Divider/>
-            <Accordion heading={"Governance"} />
-            <Accordion heading={"Staking"} />
-            <Accordion heading={"Deflationary Mechanics"} />
-            <Accordion heading={"Liquidity Farming"} />
-        </AccordionContainer>
+      <Allocation />
 
-        
+      <AccordionContainer>
+        <Divider />
+        <Accordion heading={"Governance"} />
+        <Accordion heading={"Staking"} />
+        <Accordion heading={"Deflationary Mechanics"} />
+        <Accordion heading={"Liquidity Farming"} />
+      </AccordionContainer>
     </Container>
-  )
-}
+  );
+};
 
-export {AboutPage}
+export { AboutPage };
 
 const Container = styled.main`
-    padding-top: 50px;
+  padding-top: 50px;
 `;
 const Ceneter = styled.div`
   width: 100%;
   /* background-color: #000; */
 `;
 const AccordionContainer = styled.section`
-    padding: 50px 0;
-`
+  padding: 50px 0;
+`;
 
 const Divider = styled.div`
-    border-top: 1px solid ${colors.light_grey};
-    margin: 0 auto;
-    width: 40%;
-    padding: 25px 0;
+  border-top: 1px solid ${colors.light_grey};
+  margin: 0 auto;
+  width: 40%;
+  padding: 25px 0;
 `;
