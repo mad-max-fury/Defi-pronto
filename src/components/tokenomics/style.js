328ea393ca85e1ac2cdcf9427a836264eb3c8b72
mad-max-fury <chrisnonso8@gmail.com>
2022-05-24 16:54:24 +0100

    handlepage scroll

--- src/components/tokenomics/style.js
+++ src/components/tokenomics/style.js
@@ -9,40 +9,38 @@ export const Container = styled.div`
 `;
 
 export const TokenomicsContainer = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    flex-wrap: wrap;
-    padding-top: 54px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: wrap;
+  padding-top: 54px;
 `;
 
 export const TokenomicsBox = styled.div`
-    width: 369px;
-    height: 176px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 10px 20px;
-    flex-direction: column;
-    border: 2px solid ${colors.secondary};
-    border-radius: 20px;
+  width: 369px;
+  height: 176px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 10px 20px;
+  flex-direction: column;
+  border: 2px solid ${colors.secondary};
+  border-radius: 20px;
 
-    &:hover {
-        transition: all 0.3s ease-out;
-        cursor: pointer;
-        box-shadow: 0px 4px 24px ${colors.secondary} ;
-      }
+  &:hover {
+    transition: all 0.3s ease-out;
+    cursor: pointer;
+    box-shadow: 0px 4px 24px ${colors.secondary};
+  }
 `;
 
 export const TokenBoxTitle = styled.h6`
-    font-size: 12px;
-    font-weight: 400;
-    font-family: Monument Extended, DM Sans;
-    color: ${colors.secondary};
-
+  font-size: 12px;
+  font-weight: 400;
+  font-family: Monument Extended, DM Sans;
+  color: ${colors.secondary};
 `;
 
 export const TokenBoxDetail = styled.h3`
-    font-family: Monument Extended, DM Sans;
-    
+  font-family: Monument Extended, DM Sans;
 `;
